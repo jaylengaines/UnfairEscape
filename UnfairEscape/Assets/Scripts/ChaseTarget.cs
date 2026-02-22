306dc5e55efe33cd29c2f2c75ead5f1868a08223
ethanhandler17 <ethanhandler@gmail.com>
2026-02-22 13:18:09 -0500

    Enemy Now destroys player on collison

--- UnfairEscape/Assets/Scripts/ChaseTarget.cs
+++ UnfairEscape/Assets/Scripts/ChaseTarget.cs
@@ -6,11 +6,20 @@ public class ChaseTarget : MonoBehaviour
     public Transform target;
     NavMeshAgent agent;
 
-    void Awake() => agent = GetComponent<NavMeshAgent>();
-
+    void Awake(){
+        agent = GetComponent<NavMeshAgent>();
+    } 
     void Update()
     {
         if (target != null)
             agent.SetDestination(target.position);
+            
+    }
+
+    void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.CompareTag("Player")){
+            // destroy the player
+            Destroy(collision.gameObject);
+        }
     }
 }
