c505d5b2f3c7e6b85edb20a1a51a9aec71063547
ethanhandler17 <ethanhandler@gmail.com>
2026-03-01 15:55:41 -0500

    adada

--- UnfairEscape/Assets/Scripts/ResetState.cs
+++ UnfairEscape/Assets/Scripts/ResetState.cs
@@ -4,19 +4,6 @@ public class ResetStae : MonoBehaviour
 {
     // Resets the player back to original logic (normal movement) normal lightning
     public GameObject player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        if (player == null){
-            player = GameObject.FindGameObjectWithTag("Player");
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void ResetState(){
         player.GetComponent<PlayerMovement>().enabled = true;
         player.GetComponent<PlayerMovementIce>().enabled = false;
